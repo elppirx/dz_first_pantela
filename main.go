25fa4c467abc77c55fbaac878bec68b3534a38d6
Roman <elppir.x@gmail.com>
2024-10-05 02:16:36 +0400

    Add main.go 1

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var message string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
