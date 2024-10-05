b41853dfa8c9c3421dbaeef6be06791bb27e856f
Roman <elppir.x@gmail.com>
2024-10-05 23:45:47 +0400

    Add methods DELETE and UPDATE to main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -26,6 +27,41 @@ func GetMessage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, getResult)
 }
 
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	ids := mux.Vars(r)
+	id, err := strconv.Atoi(ids["id"])
+	if err != nil {
+		fmt.Fprintln(w, "Ошибка чтения id")
+	}
+	var message Message
+	DB.First(&message, id)
+
+	decoder := json.NewDecoder(r.Body)
+	var body Message
+	err = decoder.Decode(&body)
+	if err != nil {
+		http.Error(w, "Ошибка чтения json", http.StatusBadRequest)
+		return
+	}
+
+	message.Text = body.Text
+	DB.Save(&message)
+	fmt.Fprintf(w, "Сообщение с ID %v обновлено на %v", id, body.Text)
+}
+
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	ids := mux.Vars(r)
+	id, err := strconv.Atoi(ids["id"])
+	if err != nil {
+		fmt.Fprintln(w, "Ошибка чтения id")
+	}
+
+	var message Message
+	DB.First(&message, id)
+	DB.Delete(&message)
+	fmt.Fprintf(w, "Сообщение с ID %v удалено", id)
+}
+
 func main() {
 	InitDB()
 
@@ -35,6 +71,8 @@ func main() {
 
 	router.HandleFunc("/api/message", GetMessage).Methods("GET")
 	router.HandleFunc("/api/message", AddMessage).Methods("POST")
+	router.HandleFunc("/api/message/{id}", UpdateMessage).Methods("PUT")
+	router.HandleFunc("/api/message/{id}", DeleteMessage).Methods("DELETE")
 
 	http.ListenAndServe(":8080", router)
 }
